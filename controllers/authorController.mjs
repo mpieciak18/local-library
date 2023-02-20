86e30b54ec2a973ba866a171f01f518ffc6076bc
Mark Pieciak <mpieciak18@msn.com>
2023-02-19 21:09:51 -0500

    add delete author functionality

--- controllers/authorController.mjs
+++ controllers/authorController.mjs
@@ -132,13 +132,65 @@ export const author_create_post = [
 ];
 
 // Display Author delete form on GET.
-export function author_delete_get(req, res) {
-	res.send(`NOT IMPLEMENTED: Author delete GET`);
+export function author_delete_get(req, res, next) {
+	async.parallel(
+		{
+			author(callback) {
+				Author.findById(req.params.id).exec(callback);
+			},
+			authors_books(callback) {
+				Book.find({ author: req.params.id }).exec(callback);
+			},
+		},
+		(err, results) => {
+			if (err) return next(err);
+			if (results.author === null) {
+				// No results.
+				res.redirect('/catalog/authors');
+			}
+			// Successfull, so render.
+			res.render('author_delete', {
+				title: 'Delete Author',
+				author: results.author,
+				author_books: results.authors_books,
+			});
+		}
+	);
 }
 
 // Handle Author delete on POST.
-export function author_delete_post(req, res) {
-	res.send(`NOT IMPLEMENTED: Author delete POST`);
+export function author_delete_post(req, res, next) {
+	async.parallel(
+		{
+			author(callback) {
+				Author.findById(req.body.authorid).exec(callback);
+			},
+			authors_books(callback) {
+				Book.find({ author: req.body.authorid }).exec(callback);
+			},
+		},
+		(err, results) => {
+			if (err) return next(err);
+			// Success
+			if (results.authors_books.length > 0) {
+				// Author has books. Render in same way as for GET route.
+				res.render('author_delete', {
+					title: 'Delete Author',
+					author: results.author,
+					author_books: results.authors_books,
+				});
+				return;
+			}
+			// Author has no books. Delete object and redirect to the list of authors.
+			Author.findByIdAndRemove(req.body.authorid, (err) => {
+				if (err) {
+					return next(err);
+				}
+				// Success - go to author list
+				res.redirect('/catalog/authors');
+			});
+		}
+	);
 }
 
 // Display Author update form on GET.
--- controllers/bookinstanceController.mjs
+++ controllers/bookinstanceController.mjs
@@ -1,4 +1,8 @@
-import { default as BookInstance } from '../models/bookinstance.mjs';
+import bookinstance, {
+	default as BookInstance,
+} from '../models/bookinstance.mjs';
+import { default as Book } from '../models/book.mjs';
+import { body, validationResult } from 'express-validator';
 
 // Display list of all BookInstances.
 export function bookinstance_list(req, res) {
@@ -39,14 +43,68 @@ export function bookinstance_detail(req, res, next) {
 }
 
 // Display BookInstance create form on GET.
-export function bookinstance_create_get(req, res) {
-	res.send(`NOT IMPLEMENTED: BookInstance create GET`);
+export function bookinstance_create_get(req, res, next) {
+	Book.find({}, 'title').exec((err, books) => {
+		if (err) return next(err);
+		// Successful, so render.
+		res.render('bookinstance_form', {
+			title: 'Create Book Instance',
+			book_list: books,
+		});
+	});
 }
 
 // Handle BookInstance create on POST.
-export function bookinstance_create_post(req, res) {
-	res.send(`NOT IMPLEMENTED: BookInstance create POST`);
-}
+export const bookinstance_create_post = [
+	// Validate and sanitize fields.
+	body('book', 'Book must be specified.')
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body('imprint', 'Imprint must be specified.')
+		.trim()
+		.isLength({ min: 1 })
+		.escape(),
+	body('status').escape(),
+	body('due_back', 'Invalid date.')
+		.optional({ checkFalsy: true })
+		.isISO8601()
+		.toDate(),
+
+	// Process request after validation and sanitization.
+	(req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+		// Create a BookInstance object with escaped & trimmed data
+		const bookinstance = new BookInstance({
+			book: req.body.book,
+			imprint: req.body.imprint,
+			status: req.body.status,
+			due_back: req.body.due_back,
+		});
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values and error messages.
+			Book.find({}, 'title').exec(function (err, books) {
+				if (err) return next(err);
+				// Successful, so render.
+				res.render('bookinstance_form', {
+					title: 'Create Book Instance',
+					book_list: books,
+					selected_book: bookinstance.book_id,
+					errors: errors.array(),
+					bookinstance,
+				});
+			});
+			return;
+		}
+		// Data from form is valid.
+		bookinstance.save((err) => {
+			if (err) return next(err);
+			// Successful: redirect to new record.
+			res.redirect(bookinstance.url);
+		});
+	},
+];
 
 // Display BookInstance delete form on GET.
 export function bookinstance_delete_get(req, res) {
