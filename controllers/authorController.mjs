68cc3d29257395e38ae01745c6e1c481d3dfb8c2
Mark Pieciak <mpieciak18@msn.com>
2023-02-20 10:02:44 -0500

    add book & author update functionality

--- controllers/authorController.mjs
+++ controllers/authorController.mjs
@@ -194,11 +194,104 @@ export function author_delete_post(req, res, next) {
 }
 
 // Display Author update form on GET.
-export function author_update_get(req, res) {
-	res.send(`NOT IMPLEMENTED: Author update GET`);
+export function author_update_get(req, res, next) {
+	// Get authors and authors books.
+	async.parallel(
+		{
+			author(callback) {
+				Author.findById(req.params.id).exec(callback);
+			},
+		},
+		(err, results) => {
+			if (err) {
+				return next(err);
+			}
+			if (results.author == null) {
+				// No results.
+				const err = new Error('Author not found');
+				err.status = 404;
+				return next(err);
+			}
+			// Success.
+			res.render('author_form', { title: 'Update Author' });
+		}
+	);
 }
 
 // Handle Author update on POST.
-export function author_update_post(req, res) {
-	res.send(`NOT IMPLEMENTED: Author update POST`);
-}
+export const author_update_post = [
+	// Validate and sanitize fields.
+	body('first_name')
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage('First name must be specified.')
+		.isAlphanumeric()
+		.withMessage('First name has non-alphanumeric characters.'),
+	body('family_name')
+		.trim()
+		.isLength({ min: 1 })
+		.escape()
+		.withMessage('Family name must be specified.')
+		.isAlphanumeric()
+		.withMessage('Family name has non-alphanumeric characters.'),
+	body('date_of_birth', 'Invalid date of birth')
+		.optional({ checkFalsy: true })
+		.isISO8601()
+		.toDate(),
+	body('date_of_death', 'Invalid date of death')
+		.optional({ checkFalsy: true })
+		.isISO8601()
+		.toDate(),
+
+	// Process request after validation and sanitization.
+	(req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values/errors messages.
+			res.render('author_form', {
+				title: 'Update Author',
+				author: req.body,
+				errors: errors.array(),
+			});
+			return;
+		} else {
+			// Data from form is valid.
+			// Check if Author with same name already exists.
+			Author.findOne({
+				first_name: req.body.first_name,
+				family_name: req.body.family_name,
+				date_of_birth: req.body.date_of_birth,
+				date_of_death: req.body.date_of_death,
+			}).exec((err, found_author) => {
+				if (err) {
+					return next(err);
+				}
+				if (found_author) {
+					// Author exists, redirect to its detail page.
+					res.redirect(found_author.url);
+				} else {
+					// Create an Author object with escaped and trimmed data.
+					// Then, update the record
+					const author = new Author({
+						first_name: req.body.first_name,
+						family_name: req.body.family_name,
+						date_of_birth: req.body.date_of_birth,
+						date_of_death: req.body.date_of_death,
+					});
+					Author.findByIdAndUpdate(
+						req.params.id,
+						author,
+						{},
+						(err, theauthor) => {
+							if (err) return next(err);
+							// Successful: redirect to author detail page.
+							res.redirect(theauthor.url);
+						}
+					);
+				}
+			});
+		}
+	},
+];
